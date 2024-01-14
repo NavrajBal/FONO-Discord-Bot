0628e7cd18cbffe090145c73b1c49a5868217d24
Navraj Bal <balnavsingh@gmail.com>
2024-01-14 16:12:08 -0500

    distinguish between channel types

--- file.py
+++ file.py
@@ -15,4 +15,25 @@ async def on_ready():
 async def ping(ctx):
     await ctx.author.send("Pong!")
 
+@client.command()
+async def count(ctx):
+    await ctx.channel.send(ctx.channel)
+
+@client.command()
+async def channels(ctx):
+    dServer = ctx.guild
+    allChannels = ctx.guild.channels 
+
+    vcList = []
+
+    for c in allChannels:
+        if c.type == discord.ChannelType.voice:
+            vcList.append(c.name)
+        pass
+
+
+    await ctx.channel.send(vcList)
+
+
+
 client.run(discordToken)
