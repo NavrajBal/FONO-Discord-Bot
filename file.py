f82d7b0a582de70b9e1daddf119a92f14a48355a
Navraj Bal <balnavsingh@gmail.com>
2024-01-15 02:18:38 -0500

    adding and removing of followers implemented

--- file.py
+++ file.py
@@ -3,9 +3,19 @@ import discord
 from secret import discordToken
 from discord.ext import commands
 
-print ("Hello World")
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-client = commands.Bot(command_prefix="=", intents=discord.Intents.all())
+class settings:
+    def __init__(self, id) -> None:
+        self.id = id
+        self.isFollowing = False
+        self.minSzie = 2
+        self.followingSize = 0
+        self.following = []
+        self.delay = 10
+
+
+userPrefs = {}
 
 @client.event
 async def on_ready():
@@ -19,20 +29,70 @@ async def ping(ctx):
 async def count(ctx):
     await ctx.channel.send(ctx.channel)
 
+
+@client.command()
+async def init(ctx):
+    id = ctx.author.id
+    
+    if (id not in userPrefs):
+        userPrefs[id] = settings(id)
+        await ctx.channel.send(ctx.author)
+    else:
+        await ctx.channel.send("you are already setup")
+
+@client.command()
+async def add(ctx):
+    id = ctx.author.id
+
+    if (id not in userPrefs):
+        await ctx.channel.send("You are not setup. Try !init then add")
+    else:
+        mentionsList = ctx.message.mentions
+        for i in mentionsList:
+            userPrefs[id].following.append(i.id)
+            userPrefs[id].followingSize += 1
+            
+        userPrefs[id].isFollowing = True if userPrefs[id].followingSize >= 1 else False
+        await ctx.channel.send(userPrefs[id].following)
+
+
+@client.command()
+async def remove(ctx):
+    id = ctx.author.id
+
+    if (id not in userPrefs):
+        await ctx.channel.send("You are not setup. Try !init then add")
+    elif (userPrefs[id].followingSize == 0):
+        await ctx.channel.send("You are not following anyone")
+    else:
+        mentionsList = ctx.message.mentions
+        for i in mentionsList:
+            userPrefs[id].following.remove(i.id)
+            userPrefs[id].followingSize -= 1
+        
+        userPrefs[id].isFollowing = True if len(userPrefs[id].following) >= 1 else False
+        await ctx.channel.send(userPrefs[id].following)
+
+
 @client.command()
 async def channels(ctx):
     dServer = ctx.guild
     allChannels = ctx.guild.channels 
 
     vcList = []
+    vcDict = {}
 
     for c in allChannels:
         if c.type == discord.ChannelType.voice:
-            vcList.append(c.name)
-        pass
+            vcList.append(c.id)
 
+            vcDict[c.id] = 0
 
-    await ctx.channel.send(vcList)
+            for memb in c.members:
+                vcDict[c.id] += 1
+                await ctx.channel.send(memb)
+
+    await ctx.channel.send(vcDict)
 
 
 
